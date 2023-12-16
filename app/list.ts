8c1294e862021aa7c825a9eacf15240e88deb354
Max Kerr <richard.kerr@microsoft.com>
2023-12-16 11:57:23 -0800

    Bingo

---
+++ app/list.ts
@@ -0,0 +1,39 @@
+let squares = [
+    // Places
+    "A small town",
+    "The big city",
+
+    // About the lead
+    "Lead lost a parent",
+
+    // Filler numbers while I write the code
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+]
+
+export default squares
